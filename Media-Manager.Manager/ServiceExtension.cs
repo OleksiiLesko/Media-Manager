a9f7614ad6de7898a2466e261b70e58d979a4aa6
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-08-09 17:58:49 +0300

    Added ServiceExtension for run app with service

---
+++ Media-Manager.Manager/ServiceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Manager.Manager
+{
+    /// <summary>
+    /// Class for run project as service
+    /// </summary>
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Run media-manager service 
+        /// </summary>
+        /// <param name="host"></param>
+        public static void RunAsService(this IHost host)
+        {
+            var hostService = new ManagerService(host);
+            ServiceBase.Run(hostService);
+        }
+    }
+}
