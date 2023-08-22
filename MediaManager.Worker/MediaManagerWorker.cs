69379ac4d7bcc77f225c1096fcc2b4ca4a5258ca
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-08-22 16:20:30 +0300

    Added to worker recieved messages from RabbitMQ

--- MediaManager.Worker/MediaManagerWorker.cs
+++ MediaManager.Worker/MediaManagerWorker.cs
@@ -1,25 +1,43 @@
+using MediaManager.RabbitMQClient;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog;
+using RabbitMQ.Client;
 
 namespace MediaManager.Worker
 {
-    public class MediaWorker : BackgroundService
+    /// <summary>
+    /// Represents a background worker for managing media.
+    /// </summary>
+    public class MediaManagerWorker : BackgroundService
     {
-        private readonly ILogger<MediaWorker> _logger;
-
-        public MediaWorker(ILogger<MediaWorker> logger)
+        private readonly IRabbitMQService _rabbitMQService;
+        private readonly ILogger<MediaManagerWorker> _logger;
+        private readonly IConnection _rabbitConnection;
+        private readonly IModel _rabbitChannel;
+        /// <summary>
+        /// Initializes a new instance of the MediaManagerWorker
+        /// </summary>
+        /// <param name="logger"></param>
+        public MediaManagerWorker(ILogger<MediaManagerWorker> logger, IRabbitMQService rabbitMQService)
         {
             _logger = logger;
+            _rabbitMQService = rabbitMQService;
+            _rabbitConnection = _rabbitMQService.Connect();
+            _rabbitChannel = _rabbitConnection.CreateModel();
         }
-
+        /// <summary>
+        /// Executes the media management task asynchronously.
+        /// </summary>
+        /// <param name="stoppingToken"></param>
+        /// <returns></returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            Action<string> messageHandler = (message) =>
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(5000, stoppingToken);
-            }
+                _logger.LogInformation("Received message : {message}", message);
+            };
+            _rabbitMQService.ReceiveMessage(_rabbitChannel, messageHandler);
+            await Task.Delay(5000, stoppingToken);
         }
     }
 }
