9dcf408f5294336da94c6e2dccd0adf5d791a177
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-08-25 16:43:05 +0300

    Added Enums for CallEvent and Recordings

---
+++ MediaManager.Common/Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace MediaManager.Common
+{
+    /// <summary>
+    /// Enumeration for call event directions
+    /// </summary>
+    public enum CallDirection
+    {
+        Unknown,
+        Incoming,
+        Outcoming,
+        Internal
+    }
+    /// <summary>
+    /// Enumeration for call event media type 
+    /// </summary>
+    public enum MediaType
+    {
+        Voice,
+        Screen
+    }
+    /// <summary>
+    /// Enumeration for call event recording status
+    /// </summary>
+    public enum RecordingStatus
+    {
+        None, 
+        Recorded, 
+        NotRecorded
+    }
+}
