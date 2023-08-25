1db4f4dee4c3a4f5b240e73d779426d9ab7a4207
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-08-25 16:43:46 +0300

    Added DTOs for CallEvent and Recordings

---
+++ MediaManager.Domain/DTOs/CallEvent.cs
@@ -0,0 +1,16 @@
+﻿using MediaManager.Common;
+
+namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Represents a call event containing information about a call and its associated recordings.
+    /// </summary>
+    public class CallEvent
+    {
+        public int? CallId { get; set; }
+        public DateTime CallStartTime { get; set; }
+        public DateTime CallEndTime { get; set; }
+        public CallDirection CallDirection { get; set; }
+        public List<Recording> Recordings { get; set; }
+    }
+}
---
+++ MediaManager.Domain/DTOs/Recording.cs
@@ -0,0 +1,18 @@
+﻿using MediaManager.Common;
+
+namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Represents a recording containing information about a recorded media file.
+    /// </summary>
+    public class Recording
+    {
+        public int? RecordingId { get; set; }
+        public int? RecorderId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public MediaType MediaType { get; set; }
+        public RecordingStatus RecordingStatus { get; set; }
+        public string RecordedFilePath { get; set; }
+    }
+}
