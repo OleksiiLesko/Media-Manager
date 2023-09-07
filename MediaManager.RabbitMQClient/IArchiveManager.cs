ba48f60a49da1451e3a925b85b3fe0b3981e8e52
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2023-09-07 11:13:52 +0300

    Added interface for ArchiveManager

---
+++ MediaManager.RabbitMQClient/IArchiveManager.cs
@@ -0,0 +1,17 @@
+﻿using MediaManager.Domain.DTOs;
+
+namespace MediaManager.RabbitMQClient
+{
+    /// <summary>
+    /// Represents an archiver  for archive files recieved from RabbitMQ
+    /// </summary>
+    public interface IArchiveManager
+    {
+        /// <summary>
+        ///  Copies recording file  to a specified file path.
+        /// </summary>
+        /// <param name="callEvent"></param>
+        /// <returns></returns>
+        Task ArchiveCallEventAsync(CallEvent callEvent);
+    }
+}
