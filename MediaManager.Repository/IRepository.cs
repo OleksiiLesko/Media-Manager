359e5742139adcbfa352ff78c2bf4a212832ba21
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2023-09-21 11:12:40 +0300

    Added IRepository for work with database

---
+++ MediaManager.Repository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using MediaManager.Common;
+using MediaManager.Domain.DTOs;
+
+namespace MediaManager.Repositories
+{
+    /// <summary>
+    /// Configuration for work with database
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Works with database
+        /// </summary>
+        /// <param name="callEvent"></param>
+        void SaveCallToDatabase(CallEvent callEvent);
+        /// <summary>
+        /// Sets status archiving of recording 
+        /// </summary>
+        /// <param name="callEvent"></param>
+        void SetArchivingStatus(CallEvent callEvent, string archivingFilePath, ArchivingStatus archivingStatus);
+    }
+}
