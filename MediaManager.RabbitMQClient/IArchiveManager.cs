e623df4b8eed30b5f11335fa1798ae06943915e7
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2023-09-21 10:58:44 +0300

    Nothing changed

--- MediaManager.RabbitMQClient/IArchiveManager.cs
+++ MediaManager.RabbitMQClient/IArchiveManager.cs
@@ -1,4 +1,5 @@
-﻿using MediaManager.Domain.DTOs;
+﻿using MediaManager.Common;
+using MediaManager.Domain.DTOs;
 
 namespace MediaManager.RabbitMQClient
 {
@@ -13,5 +14,11 @@ namespace MediaManager.RabbitMQClient
         /// <param name="callEvent"></param>
         /// <returns></returns>
         Task ArchiveCallEventAsync(CallEvent callEvent);
+        /// <summary>
+        /// Constructs the archive path for a given call event based on the configuration settings and call event details.
+        /// </summary>
+        /// <param name="callEvent"></param>
+        /// <returns></returns>
+        string GetArchivePath(CallEvent callEvent);
     }
 }
