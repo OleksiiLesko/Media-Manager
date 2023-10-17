0722ca0c079c56b78e29147cb72f0e10c03ddc4f
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2023-10-17 12:48:36 +0300

    Added RecordingArchivingEvent

---
+++ MediaManager.Domain/DTOs/RecordingArchivingEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaManager.Domain.DTOs
+{
+    public class RecordingArchivingEvent
+    {
+        public int RecordingId { get; set; }
+        public DateTime StartTime { get; set; }
+        public string MediaType { get; set; }
+        public string ArchivingFilePath { get; set; }
+        public string RecordingArchivingStatus { get; set; }
+    }
+}
