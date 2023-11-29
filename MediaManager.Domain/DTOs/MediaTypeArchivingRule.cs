f7bcf7969c158c6444b8ef273001777d008cc27b
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2023-11-29 14:56:47 +0200

    Added MediaTypeArchivingRule

---
+++ MediaManager.Domain/DTOs/MediaTypeArchivingRule.cs
@@ -0,0 +1,13 @@
+﻿using MediaManager.Common;
+
+namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Archiving rule for media type.
+    /// </summary>
+    public class MediaTypeArchivingRule 
+    {
+        public bool Enabled { get; set; }
+        public List<MediaType> MediaTypes { get; set; }
+    }
+}
