21371b1e6ea27f2a3e1809162859f5ee64028f5a
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2023-11-29 14:56:10 +0200

    Added FilteredCall

---
+++ MediaManager.Domain/DTOs/FilteredCall.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Represents a filtered call by rules.
+    /// </summary>
+    public class FilteredCall : CallEvent
+    {
+    }
+}
