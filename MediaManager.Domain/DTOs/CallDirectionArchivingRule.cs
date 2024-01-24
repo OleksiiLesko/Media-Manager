4b6c09f28bd63289debc0b1917b81cf3812f6c25
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-01-24 20:13:28 +0200

    Added CallDirectionArchivingRule

---
+++ MediaManager.Domain/DTOs/CallDirectionArchivingRule.cs
@@ -0,0 +1,14 @@
+﻿using MediaManager.Common;
+
+namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Archiving rule for call direction.
+    /// </summary>
+    public class CallDirectionArchivingRule
+    {
+        public bool Enabled { get; set; }
+        public bool StopOnFailure { get; set; }
+        public List<CallDirection> CallDirections { get; set; }
+    }
+}
