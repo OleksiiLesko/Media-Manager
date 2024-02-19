4665709ad5fcefa2a2097d5495d936e603753c41
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-19 14:38:10 +0200

    Rename to CallDirectionRuleConfig

---
+++ MediaManager.Domain/DTOs/CallDirectionRuleConfig.cs
@@ -0,0 +1,15 @@
+﻿using MediaManager.Common;
+
+namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Archiving rule for call direction.
+    /// </summary>
+    public class CallDirectionRuleConfig
+    {
+        public bool Enabled { get; set; }
+        public int Priority { get; set; }
+        public bool StopOnFailure { get; set; }
+        public List<CallDirection> CallDirections { get; set; }
+    }
+}
