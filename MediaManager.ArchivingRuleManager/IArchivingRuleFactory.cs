8fe06228b229f8461fbc3fade8c9a9b1c8d5dbdd
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-19 14:32:50 +0200

    Added IArchivingRuleFactory for create all enabled rules

---
+++ MediaManager.ArchivingRuleManager/IArchivingRuleFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace MediaManager.ArchivingRuleManager
+{
+    /// <summary>
+    /// Represents a factory for archiving rules.
+    /// </summary>
+    public interface IArchivingRuleFactory 
+    {
+        /// <summary>
+        /// Creates all enabled archiving rules.
+        /// </summary>
+        /// <returns></returns>
+        List<IArchivingRule> CreateAllEnabledArchivingRules();
+    }
+}
