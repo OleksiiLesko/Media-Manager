fcd25307128dbe373aa7e9083adedfac8a8477c7
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-19 14:38:55 +0200

    Rename to CallDurationRuleConfig

---
+++ MediaManager.Domain/DTOs/CallDurationRuleConfig.cs
@@ -0,0 +1,18 @@
+﻿using MediaManager.Common;
+using System.Text.Json.Serialization;
+
+namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Archiving rule for call duration.
+    /// </summary>
+    public class CallDurationRuleConfig
+    {
+        public bool Enabled { get; set; }
+        public int Priority { get; set; }
+        public bool StopOnFailure { get; set; }
+        public string? ComparisonOperator { get; set; }
+        public int CallDurationSeconds { get; set; }
+        public ComparisonOperator ParsedComparisonOperator { get; set; }
+    }
+}
