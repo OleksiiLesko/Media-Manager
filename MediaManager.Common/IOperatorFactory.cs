e7279011e7c778d3769090189fb62492bb3508a8
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-19 14:36:56 +0200

    Added IOperatorFactory for create comparison operator

---
+++ MediaManager.Common/IOperatorFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace MediaManager.Common
+{
+    /// <summary>
+    /// Represents a factory that initializes an operator.
+    /// </summary>
+    public interface IOperatorFactory
+    {
+        /// <summary>
+        /// Creates special comparison operator.
+        /// </summary>
+        /// <param name="comparisonOperator"></param>
+        /// <returns></returns>
+        IComparisonOperator CreateOperator(ComparisonOperator comparisonOperator);
+    }
+}
