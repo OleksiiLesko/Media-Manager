65070fe6dee52c7511e16836f3d4b9b5b0bcb216
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-19 14:34:22 +0200

    Added ComparisonOperatorSettings for parse comparison operator

---
+++ MediaManager.Common/ComparisonOperatorSettings.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using System.ComponentModel;
+namespace MediaManager.Common
+{
+    /// <summary>
+    /// Represents a parser comparison operator.
+    /// </summary>
+    public static class ComparisonOperatorSettings
+    {
+        private static readonly ILogger _logger = Log.ForContext(typeof(ComparisonOperatorSettings));
+
+        /// <summary>
+        /// Parses comparison operator from configuration string format to enum.
+        /// </summary>
+        /// <param name="comparisonOperator"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static ComparisonOperator ParseComparisonOperator(string comparisonOperator)
+        {
+            var enumType = typeof(ComparisonOperator);
+            var fields = enumType.GetFields();
+
+            foreach (var field in fields)
+            {
+                var descriptionAttribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+                if (descriptionAttribute != null && descriptionAttribute.Description == comparisonOperator)
+                {
+                    var parsedOperator = (ComparisonOperator)Enum.Parse(enumType, field.Name);
+                    _logger.Information("Comparison operator parsed to : {ComparisonOperator}", parsedOperator);
+                    return parsedOperator;
+                }
+            }
+
+            throw new ArgumentException($"Comparison operator not found: {comparisonOperator}");
+        }
+    }
+}
