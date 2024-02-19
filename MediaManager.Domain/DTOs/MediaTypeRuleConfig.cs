1ac25bd083a6a84b450fdbd12c7d849baf26a903
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-19 14:39:22 +0200

    Rename to MediaTypeRuleConfig

---
+++ MediaManager.Domain/DTOs/MediaTypeRuleConfig.cs
@@ -0,0 +1,15 @@
+﻿using MediaManager.Common;
+
+namespace MediaManager.Domain.DTOs
+{
+    /// <summary>
+    /// Archiving rule for media type.
+    /// </summary>
+    public class MediaTypeRuleConfig 
+    {
+        public bool Enabled { get; set; }
+        public int Priority { get; set; }
+        public bool StopOnFailure { get; set; }
+        public List<MediaType> MediaTypes { get; set; }
+    }
+}
