3fae57a5576fecf43272880ce1510bcffaf31264
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-19 14:35:59 +0200

    Added IComparisonOperator for compare to call durations

---
+++ MediaManager.Common/IComparisonOperator.cs
@@ -0,0 +1,16 @@
+﻿namespace MediaManager.Common
+{
+    /// <summary>
+    /// Represents a comparison operator.
+    /// </summary>
+    public interface IComparisonOperator
+    {
+        /// <summary>
+        /// Compares two numbers using the comparison operator.
+        /// </summary>
+        /// <param name="callDuration"></param>
+        /// <param name="callDurationConfig"></param>
+        /// <returns></returns>
+        bool Compare(int callDuration, int callDurationConfig);
+    }
+}
