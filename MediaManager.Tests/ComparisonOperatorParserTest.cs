643816a06c07125fcad7b429aa9c14ee6616a9f3
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2024-02-21 13:58:54 +0200

    Added ComparisonOperatorParserTest

---
+++ MediaManager.Tests/ComparisonOperatorParserTest.cs
@@ -0,0 +1,26 @@
+﻿using MediaManager.Common;
+
+namespace MediaManager.Tests
+{
+    public class ComparisonOperatorParserTest
+    {
+        [Fact]
+        public void ParseComparisonOperator_ValidOperator_ReturnsCorrectEnumValue()
+        {
+            var comparisonOperatorString = ">";
+            var expectedComparisonOperator = ComparisonOperator.GreaterThan;
+
+            var actualComparisonOperator = ComparisonOperatorParser.ParseComparisonOperator(comparisonOperatorString);
+
+            Assert.Equal(expectedComparisonOperator, actualComparisonOperator);
+        }
+
+        [Fact]
+        public void ParseComparisonOperator_InvalidOperator_ThrowsArgumentException()
+        {
+            var invalidComparisonOperatorString = ">?";
+
+            Assert.Throws<ArgumentException>(() => ComparisonOperatorParser.ParseComparisonOperator(invalidComparisonOperatorString));
+        }
+    }
+}
